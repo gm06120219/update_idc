de472dde596385cd36f20bfd0d0cc0a8991b87d9
gm06120219 <gm06120219@163.com>
2015-04-09 18:31:21 +0800

    add config.json, and add a function to read config

--- config.py
+++ config.py
@@ -3,3 +3,35 @@
 
 #文件解析
 
+import os
+import json
+import math
+
+
+
+"""parse config.json"""
+def readconfig(url):
+	if url == '':
+		print "null"
+# 		file_object = open('config.json')
+# try:
+#      all_the_text = file_object.read()
+# finally:
+#      file_object.close()
+
+
+# # print all_the_text
+
+
+# decodejson = json.loads(all_the_text)
+# svr = decodejson['svr']
+# program_list = decodejson['program']
+# program_list_leng = len(program_list)
+
+# if program_list_leng>0:
+# 	for i in range(0,program_list_leng):
+# 		print program_list[i]['name']
+# 		print program_list[i]['url']
+# 	pass
+
+readconfig('')
